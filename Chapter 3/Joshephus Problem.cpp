b2c35f569f998aa02852491ca5d31ba531583306
Ryan Fisher <ryan.david.fisher@gmail.com>
2012-09-03 14:22:21 -0400

    First Commit

---
+++ Chapter 3/Joshephus Problem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+//This is another fucking comment...
+//This is not working...
+//This is another working comment....
+struct node{
+	int item;
+	node* next;
+	node (int x, node* t)	{
+		item = x;
+		next = t;
+	}
+};
+
+typedef node *link;
+
+int main(int argc, char *argv[]){
+	int i, N = atoi(argv[1]), M = atoi(argv[2]);
+	link t = new node(1,0);	t->next = t;
+	link x = t;
+	for (i = 2; i <= N; i++)	{
+		x = (x->next = new node(i,t));
+	}	while (x != x->next)	{
+		for (i = 1; i < M; i++) {
+			x = x->next;
+		}
+		x->next = x->next->next;
+	}
+	cout << x->item << endl;
+	cout << "Completed!" << endl;
+}
